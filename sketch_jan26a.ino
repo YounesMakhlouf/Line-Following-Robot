3f27d82c60f9a15ccc8a21385906586f6ce5b1b1
YounesMakhlouf <91753449+YounesMakhlouf@users.noreply.github.com>
2023-01-28 11:33:04 +0100

    code for following the black line

--- sketch_jan26a.ino
+++ sketch_jan26a.ino
@@ -4,74 +4,64 @@
 #define in3 8
 #define in4 7
 #define enB 5
-
-
-// Vitesse du moteur
-int state = 0;
+#define R_S 1
+#define L_S 2
+#define C_S 3
 
 void setup() {
-    Serial.begin(9600);
-
-    // Configuration des ports en mode "sortie"
-    pinMode(enA, OUTPUT);
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-    pinMode(enB, OUTPUT);
-    delay(400);
-
-    Serial.println("Moteur A avance vitesse maximale");
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
-    delay(500);
-    analogWrite(enA,255);
-    analogWrite(enB,255);
-    delay(200000);
-/*
-    Serial.println("Moteur B avance vitesse maximale");
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH);
-    delay(500);
-    analogWrite(enA,0);
-    analogWrite(enB,255);
-    delay(2000);
-    
-     Serial.println("Moteur A recule vitesse maximale");
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,LOW);
-    delay(500);
-    analogWrite(enA,255);
-    analogWrite(enB,0);
-    delay(2000);
-
-    Serial.println("Moteur B recule vitesse maximale");
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
-    delay(500);
-    analogWrite(enA,0);
-    analogWrite(enB,255);
-    delay(2000);
-
-    analogWrite(enA,0);
-    analogWrite(enB,255);
-    delay(100); 
-
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-    digitalWrite(in3,LOW);
-    digitalWrite(in4,LOW);*/
+  Serial.begin(9600);
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(R_S, INPUT);
+  pinMode(L_S, INPUT);
+  pinMode(C_S, INPUT);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  analogWrite(enA,255);
+  analogWrite(enB,255);
 }
 
 void loop(){
-
+  if ((L_S == 1) && (C_S == 0) && (R_S == 1))
+    forward();
+  if ((L_S == 0) && (C_S == 1) && (R_S == 1))
+    turnRight();
+  if ((L_S == 1) && (C_S == 1) && (R_S == 0))
+    turnLeft();
+  if ((L_S == 1) && (C_S == 1) && (R_S == 1))
+    stop();
 }
- 
+
+void forward(){ 
+  digitalWrite(in1, HIGH); 
+  digitalWrite(in2, LOW); 
+  digitalWrite(in3, LOW); 
+  digitalWrite(in4, HIGH);
+}
+
+void turnRight(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void turnLeft(){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void stop(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
